d79ffac8a4157a47bc04720051443700e9e7219d
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2026-07-15 23:25:29 -0700

    AXCLONE-1: Add all asteroid scenes and scripts. Still need to add instantiation on level, etc.

--- Scenes/Asteroids/asteroid_lg.gd
+++ Scenes/Asteroids/asteroid_lg.gd
@@ -1,20 +1,3 @@
 class_name Asteroid_lg extends Destructor
 
-@onready var navigateScreen = $NavigateScreenComponent
-@onready var destructionComponent = $DestructionComponent
-
-var asteroid_size = 88
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _physics_process(delta):
-	
-	#Move asteroid to other side of screen when they go off one side
-	global_position = navigateScreen.traverse_edge(get_viewport_rect().size, global_position, asteroid_size)
+var asteroid_size = 90
---
+++ Scenes/Asteroids/asteroid_md.gd
@@ -0,0 +1,3 @@
+class_name Asteroid_md extends Destructor
+
+var asteroid_size = 64
