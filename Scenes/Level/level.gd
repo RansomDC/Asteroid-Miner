e4bd0bd21e19b6a15a05e58297d2d924c0763f30
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2026-08-10 21:55:48 -0700

    AXCLONE-4: Fix respawn bug breaking game

--- Scenes/Level/level.gd
+++ Scenes/Level/level.gd
@@ -1,8 +1,6 @@
 class_name Level extends Node
 
 # Reference Nodes
-@onready var player = $Player
-@onready var player_collisions_shape = $Player/PlayerArea/PlayerCollisionPoly
 @onready var playerSpawnArea = $PlayerSpawnPosition/PlayerSpawnLocation
 @onready var asteroids = $Asteroids
 @onready var lasers = $Lasers
@@ -20,6 +18,8 @@ class_name Level extends Node
 # Variables
 var _lives := 3
 var num_asteroids = 3
+var player
+var player_collision_shape
 
 var lives:
 	set(value):
@@ -31,6 +31,8 @@ var lives:
 		return _lives
 
 func _ready():
+	player = $Player
+	player_collision_shape = $Player/PlayerArea/PlayerCollisionPoly
 	ShieldBar.value = player.player_health
 	player.connect("died", _on_player_died)
 	
@@ -56,11 +58,18 @@ func _on_player_died():
 		while !playerSpawnArea.is_empty:
 			await get_tree().create_timer(0.1).timeout
 		var ps = player_ship.instantiate()
+		
+		# Connect these signals because we instantiate a new player node when the player respawns
 		ps.connect("died", _on_player_died)
 		ps.connect("laser_fired", _on_player_laser_fired)
 		ps.connect("player_hit", _on_player_player_hit)
 		ps.global_position = playerSpawnArea.global_position
+		
+		# spawn player node
 		self.add_child(ps)
+		#We need to re-assign these variables and c
+		player = get_player()
+		player_collision_shape = player.find_child("PlayerArea").find_child("PlayerCollisionPoly")
 
 func _on_player_laser_fired(laser):
 	lasers.add_child(laser)
@@ -81,6 +90,12 @@ func _on_md_asteroid_destroyed(position):
 		asteroids.add_child(a)
 		
 
+func _on_player_player_hit(health):
+	ShieldBar.value = health
+	player_collision_shape.set_deferred("disabled", true)
+	await get_tree().create_timer(0.5).timeout
+	player_collision_shape.set_deferred("disabled", false)
+	
 ###
 # Helpers
 ###
@@ -90,9 +105,7 @@ func get_random_position():
 	var v = Vector2(randf_range(0, viewport.get_visible_rect().size.x), randf_range(0, viewport.get_visible_rect().size.y))
 	return v
 
-
-func _on_player_player_hit(health):
-	ShieldBar.value = health
-	player_collisions_shape.set_deferred("disabled", true)
-	await get_tree().create_timer(0.5).timeout
-	player_collisions_shape.set_deferred("disabled", false)
+func get_player():
+	for child in self.get_children():
+		if child is Player:
+			return child
